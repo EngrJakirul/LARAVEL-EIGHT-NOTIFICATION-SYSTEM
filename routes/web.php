2aa05e1b0d13659d7f60356798728ded13160b9b
Eng-Jakirul-Hasan <89516869+Eng-Jakirul-Hasan@users.noreply.github.com>
2023-04-04 17:44:31 +0600

    Email Notification

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Notifications\EmailNotification;
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/send-notification', function () {
+    $user = User::find(1);
+    $user->notify(new EmailNotification());
+});
+
+Route::get('/send-notification', function () {
+    $user = User::find(1);
+    Notification::send($user, new EmailNotification());
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
